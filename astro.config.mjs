97fba3c4cb9e1f34dbf798235595eedb5004ef69
Nuria Gomez Piedrafita <nuria@100x100.net>
2025-06-12 17:42:20 +0200

    wip book detail and search

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({ mode: "standalone" }),
   vite: {
     plugins: [tailwindcss()],
   },
@@ -14,16 +12,16 @@ export default defineConfig({
     enabled: false,
   },
   env: {
-    schema: {
-      SHOW_BUY_BUTTON: envField.boolean({
-        default: true,
-        context: "server",
-        access: "public",
-      }),
-      SCORE_API_ENDPOINT: envField.string({
-        context: "server",
-        access: "public",
-      }),
-    },
+    // schema: {
+    //   SHOW_BUY_BUTTON: envField.boolean({
+    //     default: true,
+    //     context: "server",
+    //     access: "public",
+    //   }),
+    //   SCORE_API_ENDPOINT: envField.string({
+    //     context: "server",
+    //     access: "public",
+    //   }),
+    // },
   },
 });
