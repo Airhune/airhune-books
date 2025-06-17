e9823c64b2bdcc9d6efac503941c6fc381921e17
Nuria Gomez Piedrafita <nuria@100x100.net>
2025-06-17 10:00:24 +0200

    add section and  hero img

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,29 @@
 // @ts-check
 import { defineConfig, envField } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import alpine from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  devToolbar: {
-    enabled: false,
-  },
-  env: {
-    // schema: {
-    //   SHOW_BUY_BUTTON: envField.boolean({
-    //     default: true,
-    //     context: "server",
-    //     access: "public",
-    //   }),
-    //   SCORE_API_ENDPOINT: envField.string({
-    //     context: "server",
-    //     access: "public",
-    //   }),
-    // },
-  },
+    output: "server",
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [alpine()],
+    devToolbar: {
+        enabled: false,
+    },
+    env: {
+        // schema: {
+        //   SHOW_BUY_BUTTON: envField.boolean({
+        //     default: true,
+        //     context: "server",
+        //     access: "public",
+        //   }),
+        //   SCORE_API_ENDPOINT: envField.string({
+        //     context: "server",
+        //     access: "public",
+        //   }),
+        // },
+    },
 });
